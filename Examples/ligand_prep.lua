1a2acd63b1179a2d16fbec92fbcb507ffa880947
shessenauer <sam.hessenauer@gmail.com>
2020-03-08 14:31:47 -0700

    up

--- Examples/ligand_prep.lua
+++ Examples/ligand_prep.lua
@@ -4,27 +4,27 @@
 
 function main ()
 	-- hide and deselect OG ligand, splits ligand, duplicates it, gives it a pretty name, aligns it into the pocket, change ligand to wire, color by carbonite
-
-
--- save the current selected atoms
-    selectedLigand = Selection_GetResidues();
-
--- get the name of the current complex
-	ligandComplex = Residue_GetComplex(selectedLigand);
-	currentComplexName = Complex_GetName(ligandComplex);
-	Command_Notification("Complex Name", currentComplexName);
-
-
--- change the selected atoms to wire display
--- hide the selected atoms original ligand
--- duplicate the selected atoms
--- get the name of the duplicated complex (ligand)
--- change the name of the new complex (ligand)
--- align the new complex (ligand) with the original complex
--- duplicate the frame of the new complex ligand
--- change the color schema of the ligand frame to something else (carbonite)
--- switch to medchem toolkit
-
-    Command_Notification("Ligand Preparation Complete");
-    return "SUCCESS";
-end;
+	
+	
+	-- save the current selected atoms
+		selectedLigand = Selection_GetResidues();
+		
+	-- get the name of the current complex
+		ligandComplex = Residue_GetComplex(selectedLigand[0]);
+		currentComplexName = Complex_GetName(ligandComplex);
+		Command_Notification(currentComplexName);
+	
+	-- change the selected atoms to wire display
+		ligandComplex
+	-- hide the selected atoms original ligand
+	-- duplicate the selected atoms
+	-- get the name of the duplicated complex (ligand)
+	-- change the name of the new complex (ligand)
+	-- align the new complex (ligand) with the original complex
+	-- duplicate the frame of the new complex ligand
+	-- change the color schema of the ligand frame to something else (carbonite)
+	-- switch to medchem toolkit
+	
+		-- Command_Notification("Ligand Preparation Complete");
+		return "SUCCESS";
+	end;
